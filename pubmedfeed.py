493cd946396825f81251f30f7973f2b1e3825abb
xdmbx <promatt4@gmail.com>
2026-06-01 18:02:52 -0500

    Update pubmedfeed.py

--- pubmedfeed.py
+++ pubmedfeed.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 
-DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/1510773471695863811/-oVyk2Ho8PEMOz_2UM13Yt88bd6gzu4dHOr-8UdkeXKUzlDayFxizAna6wRoeEHzuBrC")
+DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "")
 PUBMED_API_KEY = os.environ.get("PUBMED_API_KEY", "")
 
 CONDITIONS = [
